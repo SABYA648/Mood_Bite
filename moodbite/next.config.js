041a95e922e8431285e0b50d8732aaa54c8dd491
KHAL_SABYA <83810572+SABYA648@users.noreply.github.com>
2025-04-08 21:58:34 +0530

    Minor upg 1

---
+++ moodbite/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If you ever use images or APIs, include those configs here.
+};
+
+module.exports = nextConfig;
